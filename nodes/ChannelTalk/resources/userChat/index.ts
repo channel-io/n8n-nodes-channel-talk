67136db8734020115ace4355495f1879a9739208
kemi-yu <kemi@channel.io>
2026-02-02 15:22:54 +0900

    fix: lint

--- nodes/ChannelTalk/resources/userChat/index.ts
+++ nodes/ChannelTalk/resources/userChat/index.ts
@@ -1,6 +1,5 @@
 import type { INodeProperties } from 'n8n-workflow';
 import { userChatOpenDescription } from './open';
-import { userChatListDescription } from './list';
 
 const showOnlyForUserChats = {
 	resource: ['userChat'],
@@ -31,20 +30,5 @@ export const userChatDescription: INodeProperties[] = [
 		],
 		default: 'open',
 	},
-	...userChatOpenDescription,
-				name: 'List',
-				value: 'list',
-				action: 'List user chats',
-				description: 'List user chats in managed state',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/open/v5/user-chats',
-					},
-				},
-			},
-		],
-		default: 'list',
-	},
-	...userChatListDescription,
+	...userChatOpenDescription
 ];
